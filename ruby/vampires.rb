7cc6929893ae37ceb646ffcf83ec55550b97653e
adamscottbaxter <adamscottbaxter@gmail.com>
2016-05-03 22:28:40 -0500

    Add first two questions to vampire survey

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,13 +2,29 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 
 puts "What year were you born?"
-year = gets.chomp
+year = gets.chomp.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
 bread = gets.chomp
 
-puts "Would you like to enroll in the company’s health insurance?"
+puts "Would you like to enroll in the company’s health insurance? (yes/no)"
 insurance = gets.chomp
+
+if age + year == 2016 || age + year == 2015
+  age_correct = true
+else
+  age_correct = false
+end
+
+if age_correct && bread == "yes"
+  result = "Probably not a vampire"
+elsif age_correct == false && (bread == "no" || insurance == "no")
+  result = "Probably not a vampire"
+else
+  result = "Results inconclusive"
+end
+
+print result
