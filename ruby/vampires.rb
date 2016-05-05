c952c2f934279c4327e1760d59bb1219c375e17d
adamscottbaxter <adamscottbaxter@gmail.com>
2016-05-05 09:58:58 -0500

    Add while loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,34 +1,47 @@
-puts "What is your name?"
-name = gets.chomp
+counter = 1
 
-puts "How old are you?"
-age = gets.chomp.to_i
+puts "How many employees will be processed?"
+num_employees = gets.chomp.to_i
 
-puts "What year were you born?"
-year = gets.chomp.to_i
+while counter <= num_employees do
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
-bread = gets.chomp
-
-puts "Would you like to enroll in the company’s health insurance? (yes/no)"
-insurance = gets.chomp
-
-if age + year == 2016 || age + year == 2015
-  age_correct = true
-else
-  age_correct = false
-end
-
-if name == "Drake Cula" || name == "Tu Fang"
-  result = "Definitely a vampire"
-elsif age_correct && bread == "yes"
-  result = "Probably not a vampire"
-elsif age_correct == false && (bread == "no" || insurance == "no")
-  result = "Probably not a vampire"
-elsif age_correct == false && bread == "no" && insurance == "no"
-  result = "Almost certainly a vampire"
-else
+  puts "What is your name?"
+  name = gets.chomp
+  
+  puts "How old are you?"
+  age = gets.chomp.to_i
+  
+  puts "What year were you born?"
+  year = gets.chomp.to_i
+  
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
+  bread = gets.chomp
+  
+  puts "Would you like to enroll in the company’s health insurance? (yes/no)"
+  insurance = gets.chomp
+  
+  if age + year == 2016 || age + year == 2015
+    age_correct = true
+  else
+    age_correct = false
+  end
+  
+  if name == "Drake Cula" || name == "Tu Fang"
+    result = "Definitely a vampire"
+  elsif age_correct && bread == "yes"
+    result = "Probably not a vampire"
+  elsif age_correct == false && (bread == "no" || insurance == "no")
+    result = "Probably not a vampire"
+  elsif age_correct == false && bread == "no" && insurance == "no"
+    result = "Almost certainly a vampire"
+  else
   result = "Results inconclusive"
+  end
+  
+  puts result
+  
+  counter += 1
+
 end
 
-puts result
+
