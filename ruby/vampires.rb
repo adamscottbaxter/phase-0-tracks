82a2d5bd966fffcad230401e913df0f96fc5d6e8
adamscottbaxter <adamscottbaxter@gmail.com>
2016-05-05 20:45:59 -0500

    Add allergy check

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -9,38 +9,48 @@ while counter <= num_employees do
   name = gets.chomp
   
   puts "How old are you?"
-  age = gets.chomp.to_i
+  age = gets.to_i
   
   puts "What year were you born?"
-  year = gets.chomp.to_i
+  year = gets.to_i
   
   puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
   bread = gets.chomp
   
   puts "Would you like to enroll in the company’s health insurance? (yes/no)"
   insurance = gets.chomp
-  
-  if age + year == 2016 || age + year == 2015
-    age_correct = true
-  else
-    age_correct = false
+
+  allergy = ""
+  until allergy == "sunshine" || allergy == "done"
+    puts "Please list your allergies one at a time. Type 'done' when finished."
+    allergy = gets.chomp
   end
-  
-  if name == "Drake Cula" || name == "Tu Fang"
-    result = "Definitely a vampire"
-  elsif age_correct && bread == "yes"
-    result = "Probably not a vampire"
-  elsif age_correct == false && (bread == "no" || insurance == "no")
-    result = "Probably not a vampire"
-  elsif age_correct == false && bread == "no" && insurance == "no"
-    result = "Almost certainly a vampire"
+  if allergy == "sunshine"
+    puts "Probably a vampire."
+    counter += 1
   else
-  result = "Results inconclusive"
+    if age + year == 2016 || age + year == 2015
+      age_correct = true
+    else
+      age_correct = false
+    end
+    
+    if name == "Drake Cula" || name == "Tu Fang"
+      result = "Definitely a vampire"
+    elsif age_correct && bread == "yes"
+      result = "Probably not a vampire"
+    elsif age_correct == false && (bread == "no" || insurance == "no")
+      result = "Probably not a vampire"
+    elsif age_correct == false && bread == "no" && insurance == "no"
+      result = "Almost certainly a vampire"
+    else
+      result = "Results inconclusive"
+    end
+    
+    puts result
+    
+    counter += 1
   end
-  
-  puts result
-  
-  counter += 1
 
 end
 
