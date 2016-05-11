91d4cf2b7d5a827cda5f1a6241a2236f03dba59f
adamscottbaxter <adamscottbaxter@gmail.com>
2016-05-10 21:20:09 -0500

    Add pseudocode for 5.3

---
+++ ruby/hashes.rb
@@ -0,0 +1,24 @@
+=begin
+
+create new hash with client data for:
+client's name - string
+number of children - integer
+decor theme - string
+number of rooms - integer
+speaks english - boolean
+
+print hash when all data entered
+
+ask user "update needed?" one time
+may update one value by entering key first, then gets value
+may need to convert value to i or s as needed (for key = .. to_i???)
+update key value
+
+else "none" to skip
+
+print latest version of hash
+
+exit program
+  
+=end
+
