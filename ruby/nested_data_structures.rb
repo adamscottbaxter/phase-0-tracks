db4b37cf7e4352a454fc761cfe7e8cda023bad2d
adamscottbaxter <adamscottbaxter@gmail.com>
2016-05-13 20:22:17 -0500

    Add nested index access

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,35 +1,30 @@
 # store = {
 #   department: {
-#       name: 'Dairy',
-#       aisles: 3,
-#       sale_items: [
-#         'Eggs',
-#         'Parmesan Cheese',
-#         'Butter'
-#     ]
-#   }
+#      name: 'Dairy',
+#      aisles: 3,
+#      sale_items: [
+#        'Eggs',
+#        'Parmesan Cheese',
+#        'Butter'
+#        ]
+#    }
 # }
-
-
-# store[department]
-# upcoming items as an array value in hash
 
 store = {
   Dairy: {
     milk: {
       reg_price: '3',
       sale_price: '2'
+      },
+    eggs: {
+      reg_price: '$3',
+      sale_price: '$2'
+      },
+    butter: {
+      reg_price: '$3',
+      sale_price: '$2'
       }
     },
-    eggs: [
-      reg_price: '$3',
-      sale_price: '$2'
-      ],
-    butter: [
-      reg_price: '$3',
-      sale_price: '$2'
-      ],
-    ],
   Produce: [
     'apples',
     'carrots'
@@ -40,8 +35,9 @@ store = {
     ]
 }
 
-puts store
+puts store[:'Dairy'][:'butter'][:'sale_price']
 
-# puts store[:Dairy][:milk][:reg_price]
-# puts store[:Dairy][:sale_items][0]
-# puts store['Dairy']['sale_items'][1]
+puts store[:'Produce'].last
+
+puts store[:'Bakery'][0]
+
