18c29c1602d15a87d02cbd7dd833023f3aac5d44
adamscottbaxter <adamscottbaxter@gmail.com>
2016-05-13 22:50:18 -0500

    Finish release 0: create name_changer

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -34,25 +34,24 @@ def next_letter(z)
 end
   
 
-# def name_changer(agent_name)
-#   vowels = ['a', 'e', 'i', 'o', 'u']
-#   consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
-#   agent_name.downcase!
-#   swap_name = agent_name.split(' ')
-#   swap_name[0], swap_name[1] = swap_name[1], swap_name[0]
-#   alpha_fragment = swap_name[0].split('')
-#   beta_fragment = swap_name[1].split('')
-#   alpha_fragment.map! do |letter| 
-#     if vowels.include? letter
-#       letter = vowels[vowels.index(letter) + 1]
-#       p letter
-#     elsif consonants.include? letter
-#       letter = consonants[consonants.index(letter) + 1]
-#       p letter
-#     end
-#   end
-# p alpha_fragment
-# end
+def name_changer(agent_name)
+  agent_name.downcase!
+  swap_name = agent_name.split(' ')
+  swap_name[0], swap_name[1] = swap_name[1], swap_name[0]
+  alpha_fragment = swap_name[0].split('')
+  beta_fragment = swap_name[1].split('')
 
-p next_letter("z")
+  alpha_fragment.map! do |item|
+    next_letter(item)
+  end
+
+  beta_fragment.map! do |item|
+    next_letter(item)
+  end
+
+  sneaky_name = alpha_fragment.join('').capitalize! + " " + beta_fragment.join('').capitalize!
+
+end
+
+p name_changer("Felicia Torres")
 # name_changer("Felicia Torres")
