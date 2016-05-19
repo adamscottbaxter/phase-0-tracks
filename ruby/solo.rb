b71a8007524cc58448a284b03bbb7aaefa6c50ba
adamscottbaxter <adamscottbaxter@gmail.com>
2016-05-19 14:28:57 -0500

    Add UI

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -35,20 +35,16 @@ class Sports_team
   end
 
   def new_captain(captain_name)
-    @player_list = @player_list - [captain_name]
-    @player_list.insert(0, captain_name)
-    @player_list.each do |i|
-      if i == @player_list[0]
-        puts "#{i} <-- Captain"
-      else
-        puts "#{i}"
+      @player_list = @player_list - [captain_name]
+      @player_list.insert(0, captain_name)
+      @player_list.each do |i|
+        if i == @player_list[0]
+          puts "#{i} <-- Captain"
+        else
+          puts "#{i}"
+        end
       end
-    end
-  end
-
-  def set_record(w, l)
-    @wins = w
-    @losses = l
+    
   end
 
   def playoff_bound
@@ -56,9 +52,38 @@ class Sports_team
   end
 end
 
-bears = Sports_team.new("Bears", 4, 3)
-bears.new_captain("Elaine")
+# bears = Sports_team.new("Bears", 4, 3)
+# bears.new_captain("Stephanie")
 
-bears.playoff_bound
-bears.set_record(5,9)
-bears.playoff_bound
+# bears.playoff_bound
+# bears.losses = 22
+# bears.playoff_bound
+# bears.wins = 23
+# bears.playoff_bound
+
+all_teams = []
+checker = ""
+puts "Let's create a new Sports Team."
+until checker == "no"
+
+  puts "What is your team name?"
+  new_team_name = gets.chomp
+  puts "How many wins does your team have?"
+  new_wins =  gets.to_i
+  puts "How many losses does your team have?"
+  new_losses =  gets.to_i
+
+  created_team = (new_team_name = Sports_team.new("#{new_team_name}".capitalize, new_wins, new_losses))
+  all_teams << created_team 
+
+  puts "Would you like to add more teams?"
+  checker = gets.chomp
+end
+# p all_teams.each |team|  { puts "#{team.wins}" }
+
+all_teams.each do |x|
+  puts "#{x.team_name} created with a record of #{x.wins} and #{x.losses}."
+  x.playoff_bound
+  puts
+end
+
