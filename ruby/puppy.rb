7fd608521084eec67894c54fefec26114eb4e30a
adamscottbaxter <adamscottbaxter@gmail.com>
2016-05-19 14:34:09 -0500

    Create puppy class comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# hair_type: long / short
+# kid_friendly: true / false
+# needs_a_lot_of_excercise: true / false
+# color: varies
+
+# BEHAVIOR -----------------------
+# bark
+# roll_over
+# make_a_mess
