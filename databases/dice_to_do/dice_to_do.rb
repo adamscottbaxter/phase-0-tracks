fddac869312d465c8ee6a850093af230e1842893
adamscottbaxter <adamscottbaxter@gmail.com>
2016-06-05 18:47:42 -0400

    refactor roll

--- databases/dice_to_do/dice_to_do.rb
+++ databases/dice_to_do/dice_to_do.rb
@@ -1,31 +1,10 @@
 # Dice_to_do
-# A to-do list app with a random twist
-# for each in list ask if they want to update that item
-# require gems
 require 'SQLite3'
 
-# create SQLite3 database
 list = SQLite3::Database.new("list.db")
 list.results_as_hash = true
 rolls = SQLite3::Database.new("rolls.db")
-# rolls.results_as_hash = true
-# learn about fancy string delimiters
-# create_list_table_cmd = <<-SQL
-#   CREATE TABLE IF NOT EXISTS list (
-#     id INTEGER PRIMARY KEY,
-#     task1 VARCHAR(255),
-#     task2 VARCHAR(255),
-#     task3 VARCHAR(255),
-#     task4 VARCHAR(255),
-#     task5 VARCHAR(255),
-#     task6 VARCHAR(255),
-#     task7 VARCHAR(255),
-#     task8 VARCHAR(255),
-#     task9 VARCHAR(255),
-#     task10 VARCHAR(255),
-#     task11 VARCHAR(255)
-#   );
-# SQL
+
 
 create_list_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS list (
@@ -33,13 +12,6 @@ create_list_table_cmd = <<-SQL
     task VARCHAR(255)
   );
 SQL
-# create_list_table_cmd = <<-SQL
-#   CREATE TABLE IF NOT EXISTS list (
-#     id INTEGER PRIMARY KEY,
-#     task_number INT,
-#     task_name VARCHAR(255)
-#   );
-# SQL
 
 create_roll_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS rolls (
@@ -47,6 +19,7 @@ create_roll_table_cmd = <<-SQL
     roll INT
   );
 SQL
+
 def dice_roll
   die = [1,2,3,4,5,6] 
   x = die.sample
@@ -54,10 +27,14 @@ def dice_roll
   return x + y
 end
 
-
-# def create_list(db, task)
-#   db.execute("INSERT INTO list (task) VALUES (?)", [task])
-# end
+def roll(db)
+  puts "Alright, time to roll"
+  n = dice_roll
+  puts "just rolled a #{n}..."
+  selection = db.execute("SELECT task FROM list WHERE id=#{n-1}")
+  to_do = selection[0]['task']
+  puts "Your task today is: #{to_do}."
+end
 
 def create_whole_list(db, item)
   item.each do |i|
@@ -106,9 +83,9 @@ if list.execute("SELECT * FROM list WHERE id=1").length < 1
       create_whole_list(list, task_array)
   else
     create_whole_list(list, ["clean the gutters", "weed the garden", "wash the car", "mow the lawn", "clean the bathroom" , "clean the kitchen", "vacuum the carpet", "sweep the floors", "clean the refrigerator", "give the dog a bath", "wash the windows"])
-    # create_list(list, "dust")
-    # create_list(list, "mop")
   end
+  puts "List created, time to roll."
+  roll(list)
 else
   puts "It looks like a list already exists, would you like to (u)pdate the list or get (r)olling?"
   choice = gets.chomp
@@ -125,62 +102,24 @@ else
       print_list = list.execute("SELECT id, task FROM list")
       puts print_list
     end
-    puts "Alright, time to roll"
-    n = dice_roll
-    puts "just rolled a #{n}..."
-    selection = list.execute("SELECT task FROM list WHERE id=#{n-1}")
-    to_do = selection[0]['task']
-    puts "Your task today is: #{to_do}."
+    # puts "Alright, time to roll"
+    # n = dice_roll
+    # puts "just rolled a #{n}..."
+    # selection = list.execute("SELECT task FROM list WHERE id=#{n-1}")
+    # to_do = selection[0]['task']
+    # puts "Your task today is: #{to_do}."
+
+    roll(list)
 
   else
-    n = dice_roll
-    puts "just rolled a #{n}..."
-    selection = list.execute("SELECT task FROM list WHERE id=#{n-1}")
-    to_do = selection[0]['task']
-    puts "Your task today is: #{to_do}."
-
+    # n = dice_roll
+    # puts "just rolled a #{n}..."
+    # selection = list.execute("SELECT task FROM list WHERE id=#{n-1}")
+    # to_do = selection[0]['task']
+    # puts "Your task today is: #{to_do}."
+    roll(list)
   end
 
 end
 
-# print_list = list.execute("SELECT * FROM list")
 
-
-# print_list[0].each_pair do |key, value|
-#   if key.is_a? Integer
-#     if key == 0
-#       next
-#     else
-#       puts "#{key} has the value #{value}"
-#     end
-#   else
-#     next
-#   end
-# end
-
-# print_list[0].keys.each do |key, value|
-#   if key.is_a? Integer
-#     puts "#{print_list[0]} has the value #{print_list[0][key]}"
-#   else
-#     puts "not"
-#     # 
-#   end
-# end
-# print_list[0].each { |k,v| puts "#{key} has the value #{value}"}
-# for i in [1..2] do
-#   puts "#{print_list[0][i]} is the value"
-# end
-# [1..3].any? do |key|
-#   print_list[0].key?(key)
-# end
-# print_list[0].each do |key, value|
-#   puts "If you roll a #{key} then the task is: #{value}"
-# end
-
-# results.each do |task|
-#  puts "#{task['task1']} is #{task['task2']}"
-# end
-# kittens = db.execute("SELECT * FROM kittens")
-# kittens.each do |kitten|
-#   puts "#{kitten['name']} is #{kitten['age']} years old."
-# end
