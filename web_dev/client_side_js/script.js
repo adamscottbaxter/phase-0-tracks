ea624055ca164df217df3d69b338f29f377d686b
adamscottbaxter <adamscottbaxter@gmail.com>
2016-06-07 20:58:57 -0500

    add content to js file

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,9 @@
-fill in stuff here
+console.log("Hello World");
+var heads = document.getElementsByClassName('headers');
+headOne = heads[0]
+headTwo = heads[1]
+headOne.style.border = "5px dotted purple";
+headTwo.style.padding = "16px";
+headTwo.style.fontFamily = "Helvetica";
+// headTwo.style.userZoom = "3";
+headTwo.draggable = "true";
